b0b5793d27068650ed51bc3e4983a525281d541b
Dylan Richards <dylanerichards@gmail.com>
2015-03-16 17:34:01 -0400

    Add office to Users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150316202824) do
+ActiveRecord::Schema.define(version: 20150316212029) do
 
   create_table "categories", force: true do |t|
     t.string   "name"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 20150316202824) do
     t.string   "encrypted_password", limit: 128, null: false
     t.string   "confirmation_token", limit: 128
     t.string   "remember_token",     limit: 128, null: false
+    t.string   "office"
   end
 
   add_index "users", ["email"], name: "index_users_on_email"
